31a67cb0ccebd515884370b1fd1a5ded24631c10
strayfade <64668046+strayfade@users.noreply.github.com>
2025-05-05 17:21:33 -0400

    Final

--- application/src/main/java/com/noah/App.java
+++ application/src/main/java/com/noah/App.java
@@ -3,6 +3,8 @@ package com.noah;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,9 +15,17 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("messaging.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("MainForm.fxml"));
         scene = new Scene(fxmlLoader.load(), 640, 480);
+        Font.loadFont(getClass().getResourceAsStream("Montserrat.ttf"), 10);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
+
+        // Set window title and icon
+        stage.setTitle("Anonymous Messaging Application");
+        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+        stage.getIcons().add(icon);
+    
         stage.show();
     }
 
--- application/src/main/java/com/noah/MessagingController.java
+++ application/src/main/java/com/noah/MessagingController.java
@@ -1,17 +1,63 @@
 package com.noah;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
 
+import java.lang.Thread;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.*;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 public class MessagingController {
 
+    // Websocket client responsible for messaging
     private WebSocketClient socketClient;
+
+    @FXML
+    private VBox messageContainer;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private Label connectionStatus;
+
+    private ArrayList<String> messages;
+
+    public void addMessage(String messageText) {
+        Label messageLabel = new Label(messageText);
+        messageContainer.getChildren().add(messageLabel);
+    }
+
+    public void queueMessageViewportRefresh() {
+
+        // Clear viewport
+        messageContainer.getChildren().clear();
+        try {
+            for (int i = 0; i < messages.size(); i++) {
+                addMessage(messages.get(i));
+            }
+
+            // Sleep waits for messages to be populated before scrolling
+            Thread.sleep(100);
+
+            // Scroll to bottom of ScrollPane
+            Platform.runLater(() -> {
+                scrollPane.layout();
+                scrollPane.setVvalue(1.0);
+            });
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void tryConnectToSocket() {
         try {
@@ -19,17 +65,28 @@ public class MessagingController {
 
                 @Override
                 public void onOpen(ServerHandshake handshake) {
-                    System.out.println("Connected to WebSocket Server!");
+                    Platform.runLater(() -> {
+                        connectionStatus.setStyle("-fx-background-color:rgb(0, 136, 0);");
+                        connectionStatus.setText("Connected");
+                    });
                 }
 
                 @Override
                 public void onMessage(String message) {
-                    System.out.println("Received message: " + message);
+                    // Makes sure that addMessage runs on the FX application thread
+                    Platform.runLater(() -> {
+                        messages.add(message);
+                        queueMessageViewportRefresh();
+                    });
+
                 }
 
                 @Override
                 public void onClose(int code, String reason, boolean remote) {
-                    System.out.println("Connection closed. Reason: " + reason);
+                    Platform.runLater(() -> {
+                        connectionStatus.setStyle("-fx-background-color:rgb(136, 0, 0);");
+                        connectionStatus.setText("Not connected");
+                    });
                 }
 
                 @Override
@@ -39,7 +96,6 @@ public class MessagingController {
                 }
             };
 
-            // Connect to the WebSocket server
             socketClient.connect();
 
         } catch (URISyntaxException e) {
@@ -49,21 +105,31 @@ public class MessagingController {
 
     @FXML
     private TextField textInput;
+
     public void initialize() {
 
         // Tell the textInput to handleSendMessage() when the return key is pressed
         textInput.setOnAction(event -> {
             handleSendMessage();
         });
-        
+
         tryConnectToSocket();
+
+        messages = new ArrayList<>();
+        connectionStatus.setStyle("-fx-background-color:rgb(136, 0, 0);");
+        connectionStatus.setText("Not connected");
     }
 
     public void handleSendMessage() {
         String content = textInput.getText();
+
+        // Make sure there is a message
         if (content.length() > 0) {
+
+            // Clear textbox
             textInput.setText("");
-            System.out.println("User entered: " + content);
+
+            // Send the message if the socketClient is connected
             if (socketClient.getConnection().isOpen()) {
                 socketClient.send(content);
             }
